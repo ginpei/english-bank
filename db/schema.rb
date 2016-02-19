5db13263040d5ac5fee336a301ab2770c8425be4
ginpei <ginpei@gmail.com>
2016-02-19 15:34:44 -0800

    use slim instead of erb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160219225648) do
+ActiveRecord::Schema.define(version: 20160219231921) do
 
   create_table "phrases", force: :cascade do |t|
     t.text     "body"
