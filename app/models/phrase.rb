2afa740dbb680836337394a037edf7d99de03f44
ginpei <ginpei@gmail.com>
2016-02-19 15:13:41 -0800

    render phrase body as markdown

--- app/models/phrase.rb
+++ app/models/phrase.rb
@@ -1,2 +1,13 @@
 class Phrase < ActiveRecord::Base
+  def body_md
+    markdown.render(body).html_safe
+  end
+
+  private
+    def markdown
+      unless @markdown
+        @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+      end
+      @markdown
+    end
 end
